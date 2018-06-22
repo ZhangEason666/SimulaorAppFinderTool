e377cbba9dad849ed55c785c247f3b3c6e0f70a9
张衡 <zhangheng@hebiapp.com>
2018-06-22 17:15:55 +0800

    Initial Commit

---
+++ SimulaorAppFinderTool/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SimulaorAppFinderTool
+//
+//  Created by 张衡 on 2018/6/22.
+//  Copyright © 2018年 张衡. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
